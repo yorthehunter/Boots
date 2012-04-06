670ceda6333daac79ba4670542ef336b4f9fb4bc
yorthehunter <boru.design@gmail.com>
2012-04-06 17:33:27 -0400

    Bootstrap implemented, fluid/responsive design created, scripts in place

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Boots::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  match 'fluid/' => 'pages#fluid'
+
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -55,4 +57,6 @@ Boots::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  root :to => 'pages#index'
 end
